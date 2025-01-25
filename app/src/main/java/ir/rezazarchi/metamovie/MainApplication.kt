34f1725d831d916384aabc111df43b1c4ceeeb25
Reza Zarchi <reza.zarchi@gmail.com>
2025-01-25 14:29:28 +0330

    First commit

---
+++ app/src/main/java/ir/rezazarchi/metamovie/MainApplication.kt
@@ -0,0 +1,16 @@
+package ir.rezazarchi.metamovie
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.androix.startup.KoinStartup
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.dsl.koinConfiguration
+
+@OptIn(KoinExperimentalAPI::class)
+class MainApplication : Application(), KoinStartup {
+
+    override fun onKoinStartup() = koinConfiguration {
+        androidContext(this@MainApplication)
+    }
+
+}
