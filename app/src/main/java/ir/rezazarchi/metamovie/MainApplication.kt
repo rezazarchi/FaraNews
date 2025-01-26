27edb432a87f19c34eff06a02685e92b6acd1926
Reza Zarchi <reza.zarchi@gmail.com>
2025-01-26 10:35:34 +0330

    Added di modules for remote api services.

--- app/src/main/java/ir/rezazarchi/metamovie/MainApplication.kt
+++ app/src/main/java/ir/rezazarchi/metamovie/MainApplication.kt
@@ -3,6 +3,8 @@ package ir.rezazarchi.metamovie
 import android.app.Application
 import ir.rezazarchi.metamovie.core.di.jsonModule
 import ir.rezazarchi.metamovie.core.di.retrofitModule
+import ir.rezazarchi.metamovie.features.details.data.remote.di.movieDetailsApiServiceModule
+import ir.rezazarchi.metamovie.features.search.data.remote.di.searchMoviesApiServiceModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -16,6 +18,8 @@ class MainApplication : Application(), KoinStartup {
         modules(
             retrofitModule,
             jsonModule,
+            searchMoviesApiServiceModule,
+            movieDetailsApiServiceModule,
         )
     }
 
